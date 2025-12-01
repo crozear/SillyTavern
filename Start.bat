a4185eda676e098f8b483bc46a591945b796275f
Testing3 <brendan36@Live.com>
2025-12-01 07:07:01 -0600

    modified:   Start.bat 	modified:   public/script.js 	modified:   src/endpoints/backends/chat-completions.js

--- Start.bat
+++ Start.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd %~dp0
 set NODE_ENV=production
-call npm install --no-audit --no-fund --loglevel=error --no-progress --omit=dev
+REM call npm install --no-audit --no-fund --loglevel=error --no-progress --omit=dev
 node server.js %*
 
 pause
